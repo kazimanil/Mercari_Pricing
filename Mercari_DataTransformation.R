42ef307e5f0277c129d18938ca78a3aba87bb9ed
kazimanil <kazimanileren@gmail.com>
2017-12-01 14:17:53 +0300

    Introduction of the .Rmd file.

--- Mercari_DataTransformation.R
+++ Mercari_DataTransformation.R
@@ -1,20 +1,21 @@
 # Required Libraries & Functions ----
+rm(list = ls()); gc()
 library("tm")
 library("SnowballC")
 library("wordcloud")
 library("RColorBrewer")
+library("stringr")
 toSpace <- content_transformer(function (x , pattern ) gsub(pattern, " ", x))
 
 # Data Input -----
 test  <- fread(input = "test.tsv", sep = "\t")
 train <- fread(input = "train.tsv", sep = "\t")
-test  <- test[, `:=`(H1 = strsplit(x = category_name, split = "/")[[1]][1],
-										 H2 = strsplit(x = category_name, split = "/")[[1]][2],
-										 H3 = strsplit(x = category_name, split = "/")[[1]][3])]
-train <- train[, `:=`(H1 = strsplit(x = category_name, split = "/")[[1]][1],
-			 							  H2 = strsplit(x = category_name, split = "/")[[1]][2],
-			  						  H3 = strsplit(x = category_name, split = "/")[[1]][3])]
-
+test  <- test[, `:=`(H1 = str_split_fixed(string = category_name, pattern = "/", n = 3)[,1],
+										 H2 = str_split_fixed(string = category_name, pattern = "/", n = 3)[,2],
+										 H3 = str_split_fixed(string = category_name, pattern = "/", n = 3)[,3])]
+train <- train[, `:=`(H1 = str_split_fixed(string = category_name, pattern = "/", n = 3)[,1],
+											H2 = str_split_fixed(string = category_name, pattern = "/", n = 3)[,2],
+											H3 = str_split_fixed(string = category_name, pattern = "/", n = 3)[,3])]
 # EDA ----
 train[, .N, .(shipping)]          # 663100 shipping-free, 819435 shipping-paid
 train[, .N, .(item_condition_id)] # 5 different conditions, "1" is the most frequent, then the rest as numeric order.
@@ -23,3 +24,4 @@ length(unique(train$brand_name))  # 4310 different brands
 # Text Mining Process ----
 train_name <- Corpus(VectorSource(train$name))
 train_desc <- Corpus(VectorSource(train$item_description))
+# wordcloud(train_name)
